0f993a576eb1c0d6ae71fe8e2c12a7bc5dcc7730
zetmarley <zetmarley@outlook.com>
2025-08-25 18:31:06 +0700

    day one

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,50 @@
 <script setup></script>
 
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
-</template>
 
-<style scoped></style>
+  <div class="about">
+
+    <div class="about-title">
+      <h2>ОБО МНЕ</h2>
+      <svg class="arrow" width="40" height="41" viewBox="0 0 40 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M1.75897 5.63534L38.5001 39.4999" stroke="#191806" stroke-width="2"/>
+      <line x1="38.9847" y1="40" x2="38.9847" stroke="#191806" stroke-width="2"/>
+      <line x1="40.0001" y1="40" x2="6.10352e-05" y2="40" stroke="#191806" stroke-width="2"/>
+      </svg>
+    </div>
+
+    <div class="about-text">
+      <t>Привет!</t>
+      <t>Меня зовут Семён Караганов, являюсь Фулл-стак специалистом, 
+        окончил то-то-то-то, обучался там-то там то, такое то время, 
+        такому то научился</t>
+    </div>
+
+    <div class="about-specialization">
+      <t class="about-specialization__item">BACKEND-РАЗРАБОТКА</t>
+      <t class="about-specialization__item">FRONTEND-РАЗРАБОТКА</t>
+    </div>
+
+    
+
+      <div class="about-title">
+        <h2>ИНСТРУМЕНТЫ</h2>
+        <svg class="arrow" width="40" height="41" viewBox="0 0 40 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M1.75897 5.63534L38.5001 39.4999" stroke="#191806" stroke-width="2"/>
+        <line x1="38.9847" y1="40" x2="38.9847" stroke="#191806" stroke-width="2"/>
+        <line x1="40.0001" y1="40" x2="6.10352e-05" y2="40" stroke="#191806" stroke-width="2"/>
+        </svg>
+      </div>
+
+      <ul>
+        <li>Python, HTML & CSS & JS & TS</li>
+        <li>БД: PostgresQL, MySQL и т.п.</li>
+        <li>Фреймворки: Django, Vue, NestJS</li>
+        <li>DevOps: Nginx, Gunicorn, Docker и т.д.</li>
+      </ul>
+  
+
+  </div>
+
+  <img class="portrait" src="/src/image.png"/>
+</template>
