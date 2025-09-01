466fdec6d5b69a8bcb2715a4e871a0ca398c42cd
zetmarley <zetmarley@outlook.com>
2025-09-01 14:46:19 +0700

    templates beginning

---
+++ src/Contacts.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+    <div class="contacts">
+        
+    </div>
+</template>
---
+++ src/Projects.vue
@@ -0,0 +1,25 @@
+<script setup></script>
+
+<template>
+    <div class="projects">
+
+        <div class="project">
+            <img class="project-image" src="/src/empty.png" alt="">
+            <h3 class="project-title">Restaurant</h3>
+            <t class="project-about">Описание сайт умеет то умеет сё там все дела</t>
+        </div>
+
+        <div class="project">
+            <img class="project-image" src="/src/empty.png" alt="">
+            <h3 class="project-title">Footguess</h3>
+            <t class="project-about">Описание сайт умеет то умеет сё там все дела</t>
+        </div>
+
+        <div class="project">
+            <img class="project-image" src="/src/empty.png" alt="">
+            <h3 class="project-title">DRF skills</h3>
+            <t class="project-about">Описание сайт умеет то умеет сё там все дела</t>
+        </div>
+
+    </div>
+</template>
